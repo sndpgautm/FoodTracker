b8325dfdf87b6fb6c6e96beeb532020a670ef9ba
iosdev <iosdev@ety-idev-mba-01.local>
2019-01-22 16:22:46 +0200

    Initial Commit

---
+++ FoodTrackerLAB2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerLAB2
+//
+//  Created by iosdev on 22/01/2019.
+//  Copyright © 2019 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
