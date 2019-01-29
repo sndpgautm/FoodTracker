b4ce0c9565ae697fc264dd7f848ad37cfa196433
iosdev <iosdev@ety-idev-mba-01.local>
2019-01-29 18:20:47 +0200

    "Added Table view layout  and data models, created two scenes for viewing meal data and adding meals"

---
+++ FoodTrackerLAB2/MealTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MealTableViewCell.swift
+//  FoodTrackerLAB2
+//
+//  Created by iosdev on 29/01/2019.
+//  Copyright © 2019 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
